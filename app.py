9b35f92829eb6e660d73e907411fbb46f28ad26b
rapsar08-a11y <rapsar08@gmail.com>
2026-03-10 16:24:00 -0600

    app.py

--- app.py
+++ app.py
@@ -281,7 +281,7 @@ st.markdown(css_neutral, unsafe_allow_html=True)
 header_html = """
 <div class="main-header">
     <h1 class="main-title">"Yarvis "</h1>
-    <h2 class="image">"logo.png"</h2>
+    <h2 class="image"> logo.png </h2>
     <p class="subtitle">Tu asistente inteligente para documentos y planeación</p>
 </div>
 """
